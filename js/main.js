028dcab063fdd2d70a50e7d3b8b6dda76412ddaf
Andrey Zhuk <andrei.zhuk@coverwallet.com>
2022-09-23 00:57:43 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
 window.addEventListener('DOMContentLoaded', main);
 
-var percentEl;
-var valueEl;
-var circleEl;
-
-var currentPercent;
-var currentValue;
-var isRunAnimations;
-
 function main() {
-    eventHandler();
+    dashbord();
     selectGlobalVar();
+    keySlider();
+    changeMenu();
 }
 
 function selectGlobalVar() {
@@ -21,37 +15,73 @@ function selectGlobalVar() {
     currentValue = valueEl.textContent;
 }
 
-function eventHandler() {
+function dashbord() {
+    let percentEl = document.querySelector("#percent");
+    let valueEl = document.querySelector("#value");
+    let circleEl = document.querySelector("#circle");
+    let currentPercent = parseFloat(percentEl.textContent);
+    let isRunAnimations = false;
+
     document.querySelectorAll(".pp_dashboard_select_item").forEach(el => {
         if (!el.className.includes('nonselect')) {
             el.addEventListener("click", selectDashboard)
         }
     });
+
+    function selectDashboard(e) {
+        if (isRunAnimations) return;
+        isRunAnimations = true;
+        const percent = parseInt(e.target.parentNode.querySelector('.pp_dashboard_select_item_percent').textContent);
+        const value = e.target.parentNode.querySelector('.pp_dashboard_select_item_value').textContent;
+
+        valueEl.innerHTML = value;
+
+        runDasbordAnimation(percent, value);
+    }
+
+    function runDasbordAnimation(percent, value, deltaPercent) {
+        deltaPercent = deltaPercent ? deltaPercent : currentPercent - percent;
+        const step = deltaPercent > 0 ? -0.5 : 0.5
+        setTimeout(function(){
+            if (deltaPercent === 0) {
+                isRunAnimations = false;
+                return;
+            }
+            currentPercent += step;
+            deltaPercent += step;
+            percentEl.innerHTML = currentPercent.toFixed(1);
+            circleEl.style.setProperty('--value', currentPercent.toFixed(1) + '%');
+            runDasbordAnimation(percent, value, parseFloat(deltaPercent.toFixed(2)));
+        })
+    }
 }
 
-function selectDashboard(e) {
-    if (isRunAnimations) return;
-    isRunAnimations = true;
-    const percent = parseInt(e.target.parentNode.querySelector('.pp_dashboard_select_item_percent').textContent);
-    const value = e.target.parentNode.querySelector('.pp_dashboard_select_item_value').textContent;
+function keySlider() {
+    const keyslider = document.querySelector(".pp_key_slider");
 
-    valueEl.innerHTML = value;
+    let scrolSize = 0;
+    let step = 3;
+    function runSlider() {
+        setTimeout(function(){
+            keyslider.scrollLeft += step;
+            if (scrolSize == keyslider.scrollLeft) {
+                step = -step;
+            }
+            scrolSize = keyslider.scrollLeft;
 
-    runDasbordAnimation(percent, value);
+            runSlider();
+        }, 24)
+    }
+
+    runSlider();
 }
 
-function runDasbordAnimation(percent, value, deltaPercent) {
-    deltaPercent = deltaPercent ? deltaPercent : currentPercent - percent;
-    const step = deltaPercent > 0 ? -0.5 : 0.5
-    setTimeout(function(){
-        if (deltaPercent === 0) {
-            isRunAnimations = false;
-            return;
-        }
-        currentPercent += step;
-        deltaPercent += step;
-        percentEl.innerHTML = currentPercent.toFixed(1);
-        circleEl.style.setProperty('--value', currentPercent.toFixed(1) + '%');
-        runDasbordAnimation(percent, value, parseFloat(deltaPercent.toFixed(2)));
-    })
+
+function changeMenu() {
+
+    document.addEventListener("scroll", function(e){
+        console.log(e)
+        console.log(e.target.scrollingElement.scrollTop)
+
+    });
 }
